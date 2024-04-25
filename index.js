e2e0aac210de397853ab1bec8116acd06d551b55
MusnudBillahFaysal <138293528+MusnudBillahFaysal@users.noreply.github.com>
2024-04-25 12:51:56 +0600

    first commifftll

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-// miidleware 
+// miidleware  written here
 app.use(cors());
 app.use(express.json());
 
